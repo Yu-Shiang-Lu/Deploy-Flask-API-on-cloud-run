f2a454466a46a0e5d1f3b2f3a335acffd4c9608d
Yu-Shiang Lu <yu-shiang_lu@syscom.com.tw>
2024-11-04 16:36:22 +0800

    The 1st version

---
+++ SimpleAPIBackend/app/main.py
@@ -0,0 +1,17 @@
+from pyexpat.errors import messages
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return jsonify(message="Hello world")
+
+@app.route('/api/data', methods=['POST'])
+def get_data():
+    data = request.json
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)

2a9d0ccace4ba57b970ca0308b73e7cd4de265e9
Yu-Shiang Lu <yu-shiang_lu@syscom.com.tw>
2024-11-06 14:39:08 +0800

    Let's test whether the Cloud Build trigger work!

--- SimpleAPIBackend/app/main.py
+++ SimpleAPIBackend/app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify(message="Let's just try this out")
+    return jsonify(message="Let's test Cloud Build!!")
 
 @app.route('/api/data', methods=['POST'])
 def get_data():
